a23252a5a0c508e25fc4ac1f1e5b70caa42e2012
Huakun Shen <huakun.shen@huakunshen.com>
2021-11-27 04:30:22 -0500

    Add stop.sh script for Makefile

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-lines=$(docker ps --filter name=octoprint-assistant);
-n_lines=$(echo $lines | grep 'octoprint-assistant' | wc -l);
-if [[ $n_lines -eq 1 ]]; then
-    echo "Stop Container";
-    docker stop octoprint-assistant;
-fi
+bash ./stop.sh
 
 lines=$(docker ps -a --filter name=octoprint-assistant);
 n_lines=$(echo $lines | grep 'octoprint-assistant' | wc -l);
