0b20bc7d68e1ce463c25cda34dc92b564298dcbb
Huakun Shen <huakun.shen@huakunshen.com>
2021-12-18 22:34:19 -0500

    Update update.sh script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 bash ./stop.sh
 
+cp ./config/default ./config/default.bk
+
 lines=$(docker ps -a --filter name=octoprint-assistant);
 n_lines=$(echo $lines | grep 'octoprint-assistant' | wc -l);
 if [[ $n_lines -eq 1 ]]; then
@@ -12,3 +14,6 @@ docker run --rm -d -p 7000:8000 \
     --name octoprint-assistant \
     --env-file .env \
     huakunshen/octoprint-assistant;
+
+cp ./config/default.bk ./config/default
+rm ./config/default.bk
